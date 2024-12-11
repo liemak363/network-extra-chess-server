1f7632262e3b4e593386e623734e85f388baaddc
liemak363 <liemak363work@gmail.com>
2024-12-11 20:47:30 +0700

    feat: handle game request

---
+++ models/game_model.js
@@ -0,0 +1,51 @@
+const mysql = require('mysql2');
+
+// Create the connection to database
+const connection = mysql.createConnection({
+    host: process.env.database_host,
+    port: process.env.database_port,
+    user: process.env.database_user,
+    password: process.env.database_password,
+    database: process.env.database_name
+});
+
+module.exports.createGame = (name, tag, ip, port) => {
+    return new Promise((resolve, reject) => {
+        function func(err, results, fields) {
+            if (err) reject(err);
+            else resolve();
+        }
+        connection.query(
+            `INSERT INTO GAME (tag, name, IP, Port)
+            VALUES ('${tag}', '${name}', '${ip}', ${port})`,
+            func
+        )
+    })
+}
+
+module.exports.getAllGame = () => {
+    return new Promise((resolve, reject) => {
+        function func(err, results, fields) {
+            if (err) reject(err);
+            else resolve(results);
+        }
+        connection.query(
+            `SELECT * FROM GAME`,
+            func
+        )
+    })
+}
+
+module.exports.finishGame = (tag) => {
+    return new Promise((resolve, reject) => {
+        function func(err, results, fields) {
+            if (err) reject(err);
+            else resolve();
+        }
+        connection.query(
+            `DELETE FROM GAME
+            WHERE tag = '${tag}'`,
+            func
+        )
+    })
+}
